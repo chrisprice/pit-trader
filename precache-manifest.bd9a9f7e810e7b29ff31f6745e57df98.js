63fad3a293cdc93ccd22ad91556947da31d0f871
Chris Price <price.c@gmail.com>
2018-10-16 08:49:00 -0400

    Updates

---
+++ precache-manifest.bd9a9f7e810e7b29ff31f6745e57df98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f935464963c989e0178329275c428ff",
+    "url": "/pit-trader/static/media/mug.6f935464.png"
+  },
+  {
+    "revision": "e3552630d2013510a2427685ac569e87",
+    "url": "/pit-trader/static/media/monoton.e3552630.woff2"
+  },
+  {
+    "revision": "14791009e56350e05e5425a854c286df",
+    "url": "/pit-trader/static/media/lametric.14791009.png"
+  },
+  {
+    "revision": "6c13a3a36a589098356f1318602fd88c",
+    "url": "/pit-trader/static/media/ipad.6c13a3a3.png"
+  },
+  {
+    "revision": "e38357615b64c817fc4c",
+    "url": "/pit-trader/static/js/runtime~main.e3835761.js"
+  },
+  {
+    "revision": "85c3d69436ade7ccbc6a",
+    "url": "/pit-trader/static/js/main.85c3d694.chunk.js"
+  },
+  {
+    "revision": "285b0dfa0be174bab64d",
+    "url": "/pit-trader/static/js/1.285b0dfa.chunk.js"
+  },
+  {
+    "revision": "85c3d69436ade7ccbc6a",
+    "url": "/pit-trader/static/css/main.43e11de3.chunk.css"
+  },
+  {
+    "revision": "cc09ce0fd87d08c20342fff2a391b079",
+    "url": "/pit-trader/index.html"
+  }
+];
